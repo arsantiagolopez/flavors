332eb275e16cb62bcc561032855ad588b2f72921
Alexander Santiago <arsantiagolopez@gmail.com>
2022-06-07 13:16:39 -0500

    Sortby client fix.

--- web/src/components/ExploreContent/FilterSidebar.js
+++ web/src/components/ExploreContent/FilterSidebar.js
@@ -16,7 +16,7 @@ import { DeliveryOrPickup } from "./Filters/DeliveryOrPickup";
 import { Dietary } from "./Filters/Dietary";
 import { Nationality } from "./Filters/Nationality";
 import { PriceRange } from "./Filters/PriceRange";
-import { SortBy } from "./Filters/Sortby";
+import { SortBy } from "./Filters/SortBy";
 
 const FilterSidebar = ({ user }) => {
   const router = useRouter();
